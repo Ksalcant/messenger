1b5e1270b3e75aa50e6490beaa128e056c2e8084
ksalcant <ksalcant@asu.edu>
2025-12-24 01:01:44 -0500

    Updated connections and added User Entity

---
+++ backend/auth-service/src/main/java/com/example/auth_service/Entities/User.java
@@ -0,0 +1,30 @@
+package com.example.auth_service.Entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Table(
+    name = "users",
+    uniqueConstraints = @UniqueConstraint(columnNames="email")// unique constraint to avoid race conditions
+)
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String passwordHash;
+
+    private Instant createdAt = Instant.now();
+}
