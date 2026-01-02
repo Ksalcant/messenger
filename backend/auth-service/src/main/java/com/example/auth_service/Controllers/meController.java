49e6eff90cd6e7299c607043022c41ffe1bf924b
ksalcant <ksalcant@asu.edu>
2026-01-01 22:34:18 -0500

    started     chat-service

--- backend/auth-service/src/main/java/com/example/auth_service/Controllers/meController.java
+++ backend/auth-service/src/main/java/com/example/auth_service/Controllers/meController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class UserController {
+public class meController {
     @GetMapping("/me")
     public Map<String, Object> me(
         @AuthenticationPrincipal UserDetails userDetails
